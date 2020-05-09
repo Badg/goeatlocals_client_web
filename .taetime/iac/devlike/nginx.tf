db854d5643636ee5f44c73caa53fa8a3570ab6c5
Nick Badger <nbadger1@gmail.com>
2020-05-09 16:57:36 -0700

    Add nginx, svelte conainers in devlike

---
+++ .taetime/iac/devlike/nginx.tf
@@ -0,0 +1,14 @@
+module "taetime_dev-base_nginx" {
+    source = "../../../../taetime_dev/.taetime/iac/devlike/base_nginx"
+
+    aws_control_acct_id = data.aws_caller_identity.control_plane.account_id
+    host_workdir = var.host_workdir
+    project_name = var.project_name
+    container_name = "nginx"
+    docker_host = var.docker_host
+    container_network = docker_network.goeatlocals_network.name
+}
+
+resource "docker_network" "goeatlocals_network" {
+    name = var.project_name
+}
---
+++ .taetime/iac/devlike/svelte.tf
@@ -0,0 +1,10 @@
+module "taetime_dev-base_svelte" {
+    source = "../../../../taetime_dev/.taetime/iac/devlike/base_svelte"
+
+    aws_control_acct_id = data.aws_caller_identity.control_plane.account_id
+    host_workdir = var.host_workdir
+    project_name = var.project_name
+    container_name = "svelte"
+    docker_host = var.docker_host
+    container_network = docker_network.goeatlocals_network.name
+}
