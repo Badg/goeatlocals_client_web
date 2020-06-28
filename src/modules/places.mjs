580e523e9531156fa4fe329d014d98eec0f9e5e0
Nick Badger <nbadger1@gmail.com>
2020-06-27 22:07:17 -0700

    Add places from API backend on mount; remove hardcoded places

--- src/modules/places.mjs
+++ src/modules/places.mjs
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 import SlippyMapMarker from '../components/SlippyMapMarker.svelte';
 
 
-const bits64 = Math.pow(2, 64);
+const detailPageRoute = '/places'
 
 
 class Place {
@@ -11,7 +11,7 @@ class Place {
         // These are all from the constructor signature
         this.name = name;
         // Note: currently ignoring placeID and generating it randomly here
-        this.placeID = Math.floor(Math.random() * bits64);
+        this.placeID = placeID;
         this.placeLong = placeLong;
         this.placeLat = placeLat;
         this.info = info;
@@ -24,6 +24,7 @@ class Place {
         this.mapsplainer = null;
         this.pinCollection = null;
         this.markerComponent = null;
+        this.markerObject = null;
 
         // These binds are important because JS class support is lame
         this.highlight = this.highlight.bind(this);
@@ -31,6 +32,7 @@ class Place {
         this.togglePin = this.togglePin.bind(this);
         this.pin = this.pin.bind(this);
         this.unpin = this.unpin.bind(this);
+        this.unmountPlace = this.unmountPlace.bind(this);
     }
 
     mountPlace(slippyMap, mapsplainer, pinCollection) {
@@ -41,7 +43,7 @@ class Place {
         // This adds a marker to the map for the place
         let markerElement = document.createElement('div');
         markerElement.classList.add('slippymap-marker-container');
-        let marker = new mapboxgl.Marker({
+        this.markerObject = new mapboxgl.Marker({
                 element: markerElement,
                 anchor: 'bottom'
             })
@@ -51,6 +53,17 @@ class Place {
             target: markerElement,
             props: { place: this }
         });
+    }
+
+    unmountPlace() {
+        this.markerComponent.$destroy();
+        this.markerObject.remove();
+        // Note: we're resetting these, but we're not unpinning things, since
+        // pins are assigned by nodes and we want them to be retained even if
+        // we scroll out of view and overflow the cache
+        this.pinCollection = null;
+        this.mapsplainer = null;
+        this.slippyMap = null;
     }
 
     highlight() {
@@ -64,14 +77,14 @@ class Place {
     pin() {
         this.mapsplainer.pinPlace(this);
         this.markerComponent.pinMarker();
-        this.pinCollection.add(this);
+        this.pinCollection.add(this.placeID);
         this.pinned = true;
     }
 
     unpin() {
         this.mapsplainer.unpinPlace(this);
         this.markerComponent.unpinMarker();
-        this.pinCollection.delete(this);
+        this.pinCollection.delete(this.placeID);
         this.pinned = false;
     }
 
