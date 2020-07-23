d932d132514f249543cb73136051d29dfd8f1833
Nick Badger <nbadger1@gmail.com>
2020-07-23 01:42:39 -0700

    Cut: finish moving map importers to goeatlocals_mapdata_importer

--- .taetime/iac/devlike/postgis_initdb/2_gis.sh
+++ .taetime/iac/devlike/postgis_initdb/2_gis.sh
@@ -1,9 +1,4 @@
 for f in /project_src/initdb/gis/*.sql; do
     echo "$f"
-    psql -d gis -v ON_ERROR_STOP=1 -f "$f" 2>&1
-    PSQL_EXIT=$?
-    if [ $PSQL_EXIT -neq 0 ]
-    then
-        exit $PSQL_EXIT
-    fi
+    psql -d gis -v ON_ERROR_STOP=1 -f "$f" 2>&1 || exit $?
 done
